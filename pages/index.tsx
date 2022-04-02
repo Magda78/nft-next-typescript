6c37eca93e2c0ce57f6bbf5c48743cfebb1a1af2
Magda <>
2022-04-02 08:15:20 -0400

    second

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,45 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from 'next/image';
+import { Collection } from '../typings';
+import { sanityClient, urlFor } from '../sanity';
+import  Link  from 'next/link';
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[]
+}
+
+
+const Home = ({collections }: Props) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Main page
-        </h1>
-
+      <div className='flex w-full justify-between items-center border-b-2 p-2'>
+      <h1>The <span className='text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>PAPAFAM</span> NFT Market Place</h1>
+        </div>
+      
+      <main className=" bg-purple-900 flex w-full h-screen flex-1 flex-col items-center justify-center px-20 text-center">
+      
+      <div>
+        
+          {collections.map(collection => (
+            <Link href={`/nft/${collection.slug.current}`}>
+            <div className='flex flex-col justify-center items-center p-8 border-2 border-indigo-500'>
+             <div className='h-[200px] w-[200px]  flex flex-col justify-center items-center bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 mb-10'>
+                  <img src={urlFor(collection.mainImage).url()} className='cursor-pointer hover:scale-110 transition transform duration-200 easy-out'/>
+                  
+              </div>
+            <div>
+            <h1 className='text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>{collection.title}</h1>
+              <p className='text-white mt-3'>{collection.description}</p>
+              </div>
+              </div>
+            </Link>
+          ))}
+          </div>
         
       </main>
 
@@ -24,3 +49,39 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async() => {
+  const query = `
+  *[_type == "collection"] {
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+    asset
+  },
+  previewImage {
+    asset
+  },
+  slug {
+    current
+  },
+  creator-> {
+    _id,
+    name,
+    address,
+    slug {
+    current
+  }
+  }
+  }
+  `
+  const collections = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      collections
+    }
+  }
+}
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Image from 'next/image';
 import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import type { GetServerSideProps, NextPage } from 'next';
+import { sanityClient, urlFor } from '../../sanity';
+import { Collection } from '../../typings'
+import Link from 'next/link';
 
+interface Props {
+    collection: Collection
+}
 
-function NFTItemPage() {
+function NFTItemPage({collection}: Props) {
 
     const connectWithMetamask = useMetamask();
     const address = useAddress();
@@ -15,16 +22,19 @@ function NFTItemPage() {
    <div className='bg-purple-900 h-2/3 p-10 flex flex-col justify-center items-center md:h-screen'>
         <div className='h-[200px] w-[200px]  flex justify-center items-center bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 mb-10'>
             <div className='p-20 relative bg-red-800 '>
-            <Image src='/img/pic1.webp' layout='fill' objectFit='cover' className='hover:scale-110 transition transform duration-200 easy-out'/>
+            <Image src={urlFor(collection.previewImage).url()} layout='fill' objectFit='cover' className='hover:scale-110 transition transform duration-200 easy-out'/>
             </div>
            
         </div>
-        <h1 className='md:text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>PAPAFAM Apes</h1>
-        <p className='mt-5 md:text-center text-white text-md'>A Collection of PAPAFAM Apes, who live & breath React.</p>
+        <h1 className='md:text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>{collection.nftCollectionName}</h1>
+        <p className='mt-5 md:text-center text-white text-md'>{collection.description}</p>
     </div>
     < div className='mt-10 flex flex-col items-center p-5 w-full'>
         <div className='flex w-full justify-between items-center border-b-2 p-2'>
-        <h1>The <span className='text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>PAPAFAM</span> NFT Market Place</h1>
+            <Link href='/'>
+            <h1>The <span className='cursor-pointer text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>PAPAFAM</span> NFT Market Place</h1>
+            </Link>
+        
         <button onClick={() => address ? disconnect() :connectWithMetamask()} className='cursor-pointer bg-pink-500 pr-3 pl-3 pt-2 pb-2 text-white rounded-lg hover:bg-purple-500 '>{address ? 'SignOut' :'SignIn'}</button>
         
         </div>
@@ -33,10 +43,10 @@ function NFTItemPage() {
         )}
         <div className='relative h-[300px] w-[300px] mt-10'>
         
-            <Image src='/img/pic2.png' layout='fill' objectFit='contain' />
+            <Image src={urlFor(collection.mainImage).url()} layout='fill' objectFit='contain' />
         </div>
         <div className='mt-8 mb-8'>
-            <h1 className='text-4xl text-center'>The <span className='font-bold'>PAPAFAM Ape</span> Coding Club | NFT Drop</h1>
+            <h1 className='text-4xl text-center font-bold'>{collection.title}</h1>
         </div>
             <button className='mb-5 text-white cursor-pointer w-full px-3 py-3 mt-10 bg-gradient-to-r from-indigo-500 hover:via-purple-500 hover:to-pink-500 rounded-lg hover:bg-gradient-to-r hover:from-pink-500 via-purple-500 to-indigo-500'>Mint NFT</button>
  </div>
@@ -46,3 +56,48 @@ function NFTItemPage() {
 }
 
 export default NFTItemPage
+
+export const getServerSideProps: GetServerSideProps = async({params}) => {
+    const query = `
+    *[_type == "collection" && slug.current == $id][0] {
+      _id,
+      title,
+      address,
+      description,
+      nftCollectionName,
+      mainImage {
+      asset
+    },
+    previewImage {
+      asset
+    },
+    slug {
+      current
+    },
+    creator-> {
+      _id,
+      name,
+      address,
+      slug {
+      current
+    }
+    }
+    }
+    `
+    const collection = await sanityClient.fetch(query, {
+        id: params?.id
+    });
+
+    if(!collection) {
+        return {
+            notFound: true
+        }
+    }
+  
+    return {
+      props: {
+        collection
+      }
+    }
+  }
+  
